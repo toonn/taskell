e09740052431ba157b37142290aadc0ffa99e8b8
toonn <toonn@toonn.io>
2021-02-06 21:31:41 +0100

    Remove compiler not to mess with stackage snapshot

--- default.nix
+++ default.nix
@@ -1,11 +1,8 @@
 { pkgs ? import ./nixpkgs.nix
-, compiler-nix-name ? "ghc8102"
 , cross ? "gnu32"
 }:
 let crossPkgs = pkgs.pkgsCross.${cross};
 in (crossPkgs.haskell-nix.project {
-  inherit compiler-nix-name;
-
   src = pkgs.haskell-nix.cleanSourceHaskell {
     name = "taskell-source";
     src = ./.;
