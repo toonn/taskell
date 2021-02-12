aeb861cc323388e3ff6e4f1e1e1c04b972046b63
toonn <toonn@toonn.io>
2021-02-12 15:31:44 +0100

    Add overlay to build i686 using qemu

---
+++ armv6l-linux.nix
@@ -0,0 +1,74 @@
+final: prev:
+{
+   haskell-nix = prev.haskell-nix // ({
+     defaultModules = prev.haskell-nix.defaultModules ++ [
+      ({ pkgs, buildModules, config, lib, ... }:
+      let
+        withTH = import ./linux-cross.nix {
+          inherit (pkgs.stdenv) hostPlatform buildPlatform;
+          inherit (pkgs) stdenv lib writeScriptBin;
+          # qemu for linux
+          qemu = pkgs.buildPackages.qemu;
+
+#          wine = pkgs.buildPackages.winePackages.minimal;
+#          inherit (pkgs.windows) mingw_w64_pthreads;
+          inherit (pkgs) gmp;
+          # iserv-proxy needs to come from the buildPackages, as it needs to run on the
+          # build host.
+          inherit (final.buildPackages.ghc-extra-packages."${config.compiler.nix-name}".iserv-proxy.components.exes) iserv-proxy;
+          # remote-iserv however needs to come from the regular packages as it has to
+          # run on the target host.
+          inherit (final.ghc-extra-packages."${config.compiler.nix-name}".remote-iserv.components.exes) remote-iserv;
+          # we need to use openssl.bin here, because the .dll's are in the .bin expression.
+          extra-test-libs = [
+            # pkgs.rocksdb
+            pkgs.openssl.bin
+            pkgs.libffi
+            pkgs.gmp
+          ];
+        } // {
+          # we can perform testing of cross compiled test-suites by using wine.
+          # Therefore let's enable doCrossCheck here!
+          doCrossCheck = pkgs.stdenv.hostPlatform.isWindows;
+        };
+      in {
+        packages = {
+          # clock 0.7.2 needs to be patched to support cross compilation.
+          clock.patches              = pkgs.lib.optionals pkgs.stdenv.hostPlatform.isAarch32 [ ({ version, revision }: (if version == "0.7.2" then ./patches/clock-0.7.2.patch else null)) ];
+          # nix calls this package crypto
+        #   cryptonite-openssl.patches = pkgs.lib.optionals pkgs.stdenv.hostPlatform.isWindows [ ({ version, revision }: if version == "0.7" then ./patches/cryptonite-openssl-0.7.patch else null) ];
+
+        #   http-client.patches        = pkgs.lib.optionals pkgs.stdenv.hostPlatform.isWindows [ ({ version, revision }: if version == "0.5.14" then ./patches/http-client-0.5.14.patch else null) ];
+
+        #   conduit.patches            = pkgs.lib.optionals pkgs.stdenv.hostPlatform.isWindows [ ({ version, revision }: if builtins.compareVersions version "1.3.1.1" < 0 then ./patches/conduit-1.3.0.2.patch else null) ];
+        #   streaming-commons.patches  = pkgs.lib.optionals pkgs.stdenv.hostPlatform.isWindows [ ./patches/streaming-commons-0.2.0.0.patch ];
+        #   x509-system.patches        = pkgs.lib.optionals pkgs.stdenv.hostPlatform.isWindows [ ./patches/x509-system-1.6.6.patch ];
+        #   file-embed-lzma.patches    = pkgs.lib.optionals pkgs.stdenv.hostPlatform.isWindows [ ./patches/file-embed-lzma-0.patch ];
+
+          # Set all of these to [], as these form the
+          # dependency graph of the libiserv, iserv-proxy, and iserv-remote
+          # packages.  Subsequently we do not want the defaults that `withTH`
+          # `-fexternal-interpreter` would install here.  That would ultimately
+          # result in cyclic dependencies as it injects `remote-iserv` and
+          # `iserv-proxy` as a dependency into every package.
+          bytestring.setupBuildFlags = [];
+          containers.setupBuildFlags = [];
+          binary.setupBuildFlags = [];
+          filepath.setupBuildFlags = [];
+          time.setupBuildFlags = [];
+          Win32.setupBuildFlags = [];
+          libiserv.setupBuildFlags = [];
+          remote-iserv.setupBuildFlags = [];
+          directory.setupBuildFlags = [];
+          ghc-boot.setupBuildFlags = [];
+          transformers.setupBuildFlags = [];
+          ghci.setupBuildFlags = [];
+          network.setupBuildFlags = [];
+          unix.setupBuildFlags = [];
+        };
+      }// withTH
+      )
+     ];
+   });
+}
+
---
+++ linux-cross.nix
@@ -0,0 +1,79 @@
+let
+  # Here we try to figure out which qemu to use based on the host platform.
+  # This guess can be overridden by passing qemuSuffix
+  qemuByHostPlatform = hostPlatform:
+    # I'd prefer this was a dictionary lookup, with a fall through into abort,
+    # that would make this more readable I guess.  I think there is some similar
+    # mapping somewhere in haskell.nix
+    if hostPlatform.isAarch32
+    then "i686"
+    else if hostPlatform.isAarch64
+    then "aarch64"
+    else abort "Don't know which QEMU to use for hostPlatform ${hostPlatform.config}. Please provide qemuSuffix";
+in
+{ stdenv
+, lib
+, writeScriptBin
+, qemu
+, qemuSuffix ? (qemuByHostPlatform hostPlatform)
+, iserv-proxy
+, remote-iserv
+, gmp
+, extra-test-libs ? []
+, buildPlatform
+, hostPlatform
+, ...
+}:
+let
+
+  # we want this to hold only for arm (32 and 64bit) for now.
+  isLinuxCross = buildPlatform != hostPlatform && hostPlatform.isLinux && (hostPlatform.isAarch32 || hostPlatform.isAarch64);
+  qemuIservWrapper = writeScriptBin "iserv-wrapper" ''
+    #!${stdenv.shell}
+    set -euo pipefail
+    # Unset configure flags as configure should have run already
+    unset configureFlags
+    PORT=$((5000 + $RANDOM % 5000))
+    (>&2 echo "---> Starting remote-iserv on port $PORT")
+    ${qemu}/bin/qemu-${qemuSuffix} ${remote-iserv}/bin/remote-iserv tmp $PORT &
+    (>&2 echo "---| remote-iserv should have started on $PORT")
+    RISERV_PID="$!"
+    ${iserv-proxy}/bin/iserv-proxy $@ 127.0.0.1 "$PORT"
+    (>&2 echo "---> killing remote-iserve...")
+    kill $RISERV_PID
+    '';
+  configureFlags = lib.optional hostPlatform.isAarch32 "--disable-split-sections";
+  setupBuildFlags = map (opt: "--ghc-option=" + opt) ((lib.optionals isLinuxCross
+    [ "-fexternal-interpreter"
+      "-pgmi" "${qemuIservWrapper}/bin/iserv-wrapper"
+      "-L${gmp}/lib"
+         # Required to work-around https://gitlab.haskell.org/ghc/ghc/issues/15275
+    ] ++ lib.optionals hostPlatform.isAarch64 ["-fPIC"]))
+    ++ lib.optionals hostPlatform.isAarch32 (map (opt: "--gcc-option=" + opt) [ "-fno-pic" "-fno-plt" ])
+       # Also for GHC #15275
+    ++ lib.optionals hostPlatform.isAarch64 ["--gcc-option=-fPIC"];
+  qemuTestWrapper = writeScriptBin "test-wrapper" ''
+    #!${stdenv.shell}
+    set -euo pipefail
+    ${qemu}/bin/qemu-${qemuSuffix} $@*
+    '';
+  testWrapper = lib.optional isLinuxCross "${qemuTestWrapper}/bin/test-wrapper";
+
+  preCheck = lib.optionalString isLinuxCross ''
+    echo "================================================================="
+    echo "RUNNING TESTS for $name via qemu-${qemuSuffix}"
+    echo "================================================================="
+    echo "Copying extra test libraries"
+    for p in ${lib.concatStringsSep " " extra-test-libs}; do
+      find "$p" -iname '*.so*' -exec cp {} . \;
+    done
+  '';
+  postCheck = lib.optionalString isLinuxCross ''
+    echo "================================================================="
+    echo "END RUNNING TESTS"
+    echo "================================================================="
+  '';
+
+  enableShared = lib.mkDefault (!isLinuxCross);
+
+in { inherit preCheck postCheck configureFlags setupBuildFlags testWrapper enableShared; }
