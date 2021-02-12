eb946225f5ab612b7a03fe241971ab53e969b35c
toonn <toonn@toonn.io>
2021-02-12 13:02:54 +0100

    Steal angerman's setup
    Need to rebuild terminfo so it needs to be removed from
    nonReinstallablePkgs.

--- default.nix
+++ default.nix
@@ -7,4 +7,24 @@ in (crossPkgs.haskell-nix.project {
     name = "taskell-source";
     src = ./.;
   };
+
+  modules = [
+    # haddock can't find haddock m(
+    { doHaddock = false; }
+    # Remove Cabal from nonReinstallablePkgs to be able to pick Cabal-3.2.
+    { nonReinstallablePkgs = [
+      "rts" "ghc-heap" "ghc-prim" "integer-gmp" "integer-simple" "base"
+      "deepseq" "array" "ghc-boot-th" "pretty" "template-haskell"
+      # ghcjs custom packages
+      "ghcjs-prim" "ghcjs-th"
+      "ghc-boot"
+      "ghc" "Win32" "array" "binary" "bytestring" "containers"
+      "directory" "filepath" "ghc-boot" "ghc-compact" "ghc-prim"
+      # "ghci" "haskeline"
+      "hpc"
+      "mtl" "parsec" "process" "text" "time" "transformers"
+      "unix" "xhtml"
+      # "stm" "terminfo"
+    ]; }
+  ];
 }).taskell.components.exes.taskell
